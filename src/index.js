f27fab06f6192e760f98407bb2e0cbad87df2fb8
DmytroAntonenko <d.antonenko81@gmail.com>
2022-11-01 21:26:31 +0200

    6

--- src/index.js
+++ src/index.js
@@ -21,41 +21,41 @@ function onSearch(event) {
   newsApiService.query = event.currentTarget.elements.searchQuery.value;
   newsApiService.resetPage();
   newsApiService.OnFetchImages();
-  newsApiService.OnFetchImages().then(hits => console.log(hits));
+  newsApiService.OnFetchImages().then(articlesMarkup);
   }
   
-  function onLoadMore(event) {
-    newsApiService.OnFetchImages().then(hits => console.log(hits));
-  }
-// function onLoadMore(event) {
-//   newsApiService.OnFetchImages().then(articlesMarkup);
+  // function onLoadMore(event) {
+  //   newsApiService.OnFetchImages().then(hits => console.log(hits));
+  // }
+function onLoadMore(event) {
+  newsApiService.OnFetchImages().then(articlesMarkup);
   
-// }
+}
 
-// function articlesMarkup(hits) {
-//   galleryRef.insertAdjacentHTML('afterend', articlesTpl(hits))
-// }
+function articlesMarkup(hits) {
+  galleryRef.insertAdjacentHTML('afterend', articlesTpl(hits))
+}
 
-// function articlesTpl(hits) {
-//   const markup = hits
-//     .map(hit => {
-//       return `<div class="photo-card">
-//       <img src="" alt="" loading="lazy" />
-//       <div class="info">
-//         <p class="info-item">
-//           <b>Likes</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Views</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Comments</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Downloads</b>
-//         </p>
-//       </div>
-//     </div>`;
-//     })
-//     .join('');
-// }
+function articlesTpl(hits) {
+  const markup = hits
+    .map(hit => {
+      return `<div class="photo-card">
+      <img src="" alt="" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+        </p>
+      </div>
+    </div>`;
+    })
+    .join('');
+}
