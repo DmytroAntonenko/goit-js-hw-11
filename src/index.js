dc2a9635f0e7a6e84d847e5044e46c0d7363edd0
DmytroAntonenko <d.antonenko81@gmail.com>
2022-11-01 12:58:42 +0300

    4

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import SimpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-
-
-
 const searchForm = document.querySelector('#search-form');
 const inputRef = document.querySelector('input[name="searchQuery"]');
 
 let per_page = 40;
-let page = 0;
+let page = 1;
 
 searchForm.addEventListener('submit', onSearch);
 
@@ -23,10 +20,11 @@ function onSearch(event) {
     const KEY = '30992606-4305b7e2b4564aba7b063f01d';
   
     try {
-      const response = await axios.get(
+      const response = await fetch(
         `${URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
       );
-      return response.data;
+      const users = await response.json();
+      console.log(users);
     } catch (error) {
       console.log(error);
     }
