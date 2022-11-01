b573c488b0fa0ec3b7d5259145beab74d3290916
DmytroAntonenko <d.antonenko81@gmail.com>
2022-11-01 12:25:03 +0300

    3

--- src/index.js
+++ src/index.js
@@ -10,20 +10,21 @@ const inputRef = document.querySelector('input[name="searchQuery"]');
 
 let per_page = 40;
 let page = 0;
-let name = inputRef.value.trim();
-console.log(name)
 
 searchForm.addEventListener('submit', onSearch);
 
 function onSearch(event) {
   event.preventDefault();
-  async function OnFetchImages(name, page, per_page) {
+  const searchQuery = event.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery)
+  
+  async function OnFetchImages(searchQuery, page, per_page) {
     const URL = 'https://pixabay.com/api/';
     const KEY = '30992606-4305b7e2b4564aba7b063f01d';
   
     try {
       const response = await axios.get(
-        `${URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
+        `${URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
       );
       return response.data;
     } catch (error) {
